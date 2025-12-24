311300bc7a0019550eee3082b5505f4cd2ef4399
Mehmet Emin BALOĞLU <baloglu321@gmail.com>
2025-12-24 16:41:17 +0300

    retouch

--- week-1/day-1/instant.py
+++ week-1/day-1/instant.py
@@ -5,25 +5,26 @@ import os
 
 app = FastAPI()
 
+
 @app.get("/", response_class=HTMLResponse)
 def instant():
     # Uzak Ollama sunucusu URL'i (ortam değişkeninden veya varsayılan olarak)
-    ollama_base_url = os.getenv("OLLAMA_BASE_URL", "https://allocated-sphere-rec-inherited.trycloudflare.com/")
-    
+    ollama_base_url = os.getenv("OLLAMA_BASE_URL", ".../")
+
     # Uzak Ollama sunucusuna bağlan
     llm = ChatOllama(
         model="gemma3:27b",  # Ollama'da yüklü olan model adını kullanın
-        base_url=ollama_base_url
+        base_url=ollama_base_url,
     )
-    
+
     message = """
 You are on a website that has just been deployed to production for the first time!
 Please reply with an enthusiastic announcement to welcome visitors to the site, explaining that it is live on production for the first time!
 """
-    
+
     # LangChain Ollama ile mesaj gönder
     response = llm.invoke(message)
     reply = response.content.replace("\n", "<br/>")
-    
+
     html = f"<html><head><title>Live in an Instant!</title></head><body><p>{reply}</p></body></html>"
     return html
