01fb59f2897f20405398de9d4c94896166bcb2ee
Mehmet Emin BALOĞLU <baloglu321@gmail.com>
2026-01-29 17:18:12 +0300

    Integrated alex app into main repository

---
+++ week-3/day-3/alex/terraform/2_sagemaker/variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+  description = "AWS region for resources"
+  type        = string
+}
+
+variable "sagemaker_image_uri" {
+  description = "URI of the SageMaker container image"
+  type        = string
+}
+
+variable "embedding_model_name" {
+  description = "Name of the HuggingFace model to use"
+  type        = string
+  default     = "sentence-transformers/all-MiniLM-L6-v2"
+}
